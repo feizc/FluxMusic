b6eb3a0a87db94a71be526d70597a94f36a58643
费政聪 <3300949577@qq.com>
2024-08-30 01:17:18 -0500

    Update train_b.sh

--- scripts/train_b.sh
+++ scripts/train_b.sh
@@ -2,5 +2,5 @@ torchrun --nnodes=2 --nproc_per_node=8 train.py \
 --version base \
 --data-path combine_dataset.json \
 --global_batch_size 128 \
---resume results/base/checkpoints/0050000.pt \
+--resume xxx \
 --global-seed 2023
